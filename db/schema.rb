01cf66592b8f06e64484dbd69afd183d67cc2361
ghanshyam <ghanshyam.sikri@gmail.com>
2022-10-29 22:49:26 +0530

    add comment feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_29_073306) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_29_094955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_073306) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "Commenter"
+    t.text "body"
+    t.bigint "articles_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["articles_id"], name: "index_comments_on_articles_id"
+  end
+
+  add_foreign_key "comments", "articles", column: "articles_id"
 end
